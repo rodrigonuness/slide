fcb5083b49d6ea4b694e9af19fe5e6d07463db1c
Rodrigo Nunes <69399583+rodrigonuness@users.noreply.github.com>
2020-09-19 20:56:12 -0300

    Update slide.js

--- slide.js
+++ slide.js
@@ -1,52 +1,52 @@
-        var slide = document.getElementById('slide')
-        var x = 1
+        var slide = document.getElementById('slide');
+        var x = 1;
 
         change()
 
         function change(){
             switch (x) {
                 case 1:
-                slide.style.backgroundImage = "url(./i1.jpg)"
-                    x = 2
-                    break
+                slide.style.backgroundImage = "url(./i1.jpg)";
+                    x = 2;
+                    break;
                 case 2:
-                    slide.style.backgroundImage = "url(./i2.jpg)"
-                    x = 3
-                    break
+                    slide.style.backgroundImage = "url(./i2.jpg)";
+                    x = 3;
+                    break;
                 case 3:
-                    slide.style.backgroundImage = "url(./i3.jpg)"
-                    x = 4
-                    break
+                    slide.style.backgroundImage = "url(./i3.jpg)";
+                    x = 4;
+                    break;
                 case 4:
-                    slide.style.backgroundImage = "url(./i4.jpg)"
-                    x = 1
-                    break
+                    slide.style.backgroundImage = "url(./i4.jpg)";
+                    x = 1;
+                    break;
             
                 default:
-                    x = 1
-                    break
+                    x = 1;
+                    break;
             }
         }
 
-            window.setInterval(change, 3000)
+            window.setInterval(change, 3000);
 
 
             const image1 = () => {
-                slide.style.backgroundImage = "url(./i1.jpg)"
-                x = 2
+                slide.style.backgroundImage = "url(./i1.jpg)";
+                x = 2;
             }
 
             const image2 = () => {
-                slide.style.backgroundImage = "url(./i2.jpg)"
-                x = 3
+                slide.style.backgroundImage = "url(./i2.jpg)";
+                x = 3;
             }
             
             const image3 = () => {
-                slide.style.backgroundImage = "url(./i3.jpg)"
-                x = 4
+                slide.style.backgroundImage = "url(./i3.jpg)";
+                x = 4;
             }
 
             const image4 = () => {
-                slide.style.backgroundImage = "url(./i4.jpg)"
-                x = 1
+                slide.style.backgroundImage = "url(./i4.jpg)";
+                x = 1;
             }
